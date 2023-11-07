38408d747c20f36cc642a2f23005ff8ca0dd7f3e
Dhananjay Meshram <meshramdhananjay1@gmail.com>
2023-11-07 12:59:32 +0530

    Initial Commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,28 @@
-function Header(){
-    return(
-       <header>
+import { useEffect, useState } from "react";
+
+function Header(props) {
+    const [username , setUsername] = useState('');
+    useEffect(()=>{
+      async function fetchUsername(){
+        try{
+          const usern = await props.username();
+          setUsername(usern);
+        }
+        catch(err){
+          console.error(err);
+        }
+      }
+
+      fetchUsername();
+    });
+    return (
+      <header>
         <div className="h1">
-            <h2>Todo List</h2> 
+          <h2>{ username !== null ? 'Hello '+ username + ' What Todo today!😃' : 'Loding...'  }</h2>
         </div>
-       </header>
-    )
-}
-export default Header
+      </header>
+    );
+  }
+  
+  export default Header;
+  
